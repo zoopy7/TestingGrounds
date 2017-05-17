bd5487bfda420dd2f2de5d67da1ac3b02a2a9826
zoopy7 <cjdeleon7@yahoo.com>
2017-05-16 21:01:35 -0700

    Video 225 - Changing a Character's Animation
    - Change the Animation class and Skeletal Mesh to the PatrollingGuard Blueprint
    - Appended the string Old infront of a Skeletal Mesh
    - Changed PatrollingGuard UPROPERTY from EditInstanceOnly to EditAnywhere
    - Added Git LFS tracking on uasset and umap

--- Source/TestingGrounds/PatrolRoute.h
+++ Source/TestingGrounds/PatrolRoute.h
@@ -14,7 +14,7 @@ public:
 	TArray<AActor*> GetPatrolPoints() const;
 
 private:
-	UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
+	UPROPERTY(EditAnywhere, Category = "Patrol Route")
 		TArray<AActor*> PatrolPoints;
 		
 	
